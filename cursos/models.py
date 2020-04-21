81f3913ff71420b271d3b0b5c5d9294d828fb44b
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-04-20 22:33:48 -0300

    models e migrate

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=120)
+    sigla = models.CharField(max_length=5)
+    descricao = models.TextField()
